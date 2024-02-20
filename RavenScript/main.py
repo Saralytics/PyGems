771d76ad43b593decf0b7f8084e4a5425320b9ed
Saralytics <zhaoxue.li@outlook.com>
2024-02-20 20:28:45 +0400

    send to my other email address

--- RavenScript/main.py
+++ RavenScript/main.py
@@ -1,13 +1,12 @@
 import smtplib
 
 email = "saralytics@gmail.com"
-password = ""
-
+password = "oahf jzsk wlhw tapi"
 connection = smtplib.SMTP("smtp.gmail.com", port=587) # host provider
 connection.starttls()   # securing connection
 connection.login(user=email, password=password)
 connection.sendmail(
     from_addr=email,
-    to_addrs="zhaoxue.li@outlook.com",
-    msg="Subject: Your morning coffee delivery is here!\n\n This is my body of the cup")
+    to_addrs="zhaoxue.li@alumni.ie.edu",
+    msg="Subject: Did you do your reading today!\n\n Read, check what's new in the hood and write something down")
 connection.close()
